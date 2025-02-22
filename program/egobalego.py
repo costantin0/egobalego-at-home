6aa63ab17470b488bd195bd42fcb03a5f306161e
costantino2000 <86307814+costantin0@users.noreply.github.com>
2025-02-22 16:04:34 +0100

    Colored warning and error logs
    + code refactoring
    + improved socketio security

--- program/egobalego.py
+++ program/egobalego.py
@@ -3,37 +3,44 @@ from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
 from mistune import create_markdown
 
+# Flask app and SocketIO initialization
 app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO()
 
+# Argument parser
 parser = argparse.ArgumentParser()
 parser.add_argument("--open", action='store_true', help="Open browser page on start")
 parser.add_argument("-P", "--port", type=int, default=5000, help="Server port")
 parser.add_argument("--debug", action=argparse.BooleanOptionalAction, default=True, help="Flask debug mode")
 parser.add_argument("--lang", default="en_us", help='Website language')
 
-dirname = os.path.dirname(__file__)
-data_folder = os.path.join(dirname, "..", "data")
-app_version_file = os.path.join(data_folder, "app_version")
-translations_file = os.path.join(data_folder, "translations.json")
-server_data_file = os.path.join(data_folder, "server_data.json")
-last_id_file = os.path.join(data_folder, "last_id.txt")
-color_theme_file = os.path.join(data_folder, "color_theme")
+# Constants
+FOLDER_PROGRAM = os.path.dirname(__file__)
+FOLDER_DATA = os.path.join(FOLDER_PROGRAM, "..", "data")
+FILE_APP_VERSION = os.path.join(FOLDER_DATA, "app_version")
+FILE_TRANSLATIONS = os.path.join(FOLDER_DATA, "translations.json")
+FILE_SERVER_DATA = os.path.join(FOLDER_DATA, "server_data.json")
+FILE_LAST_ID = os.path.join(FOLDER_DATA, "last_id.txt")
+FILE_COLOR_THEME = os.path.join(FOLDER_DATA, "color_theme")
+UPDATE_CHECK_URL = "https://raw.githubusercontent.com/costantin0/egobalego-at-home/refs/heads/main/data/app_version"
+THEME_LIGHT = "light"
+THEME_DARK = "dark"
+COLOR_WARNING = '\033[93m'
+COLOR_ERROR = '\033[91m'
+COLOR_ENDC = '\033[0m'
 
-LIGHT_THEME = "light"
-DARK_THEME = "dark"
-
+# Global variables
 translations = {}
 server_data = []
 last_id = 0
-color_theme = LIGHT_THEME
+color_theme = THEME_LIGHT
 local_version = 0
 github_version = 0
 update_available = False
 lang = ""
 
 
-# App Routes
+#region App Routes
 
 @app.route('/')
 def home():
@@ -48,7 +55,7 @@ def trades():
     return render_custom_template('trades')
 
 @app.route('/communications.html')
-def messages():
+def communications():
     return render_custom_template('communications')
 
 @app.route('/quest-steps.html')
@@ -99,12 +106,14 @@ def send_last_id():
 @app.route('/switch_theme', methods=['GET'])
 def switch_color_theme():
     global color_theme
-    color_theme = DARK_THEME if color_theme == LIGHT_THEME else LIGHT_THEME
+    color_theme = THEME_DARK if color_theme == THEME_LIGHT else THEME_LIGHT
     update_color_theme()
     return "Theme was switched!"
 
+#endregion
 
-# SocketIO Events
+
+#region SocketIO Events
 
 mod_connected_to_socket = False
 
@@ -148,30 +157,35 @@ def send_command(data):
 
 @socketio.on('mod_response')
 def handle_mod_response(response):
-    print('Received response from the mod: ' + response)
+    print("Received response from the mod:", response)
     emit("mod_response", response, broadcast=True)
 
+#endregion
 
-# Misc functions
+
+#region Misc functions
 
 def load_data():
     global server_data
     try:
-        with open(server_data_file, 'r') as f:
+        with open(FILE_SERVER_DATA, 'r') as f:
             data = f.read()
             server_data = json.loads(data)
     except FileNotFoundError:
-        print(f"Could not find the file {server_data_file}, it will be created the first time you add something.")
+        print_warning(f"Could not find the file '{FILE_SERVER_DATA}', it will be created the first time you add something.")
     except Exception as e:
-        print("Server data could not be loaded and was reset, changes will apply on the next edit:", e)
+        print_error(f"Server data could not be loaded and was reset, changes will apply on the next edit:\n{e}")
 
 def update_data():
     received_data = request.json
-    if "add" in received_data.keys():
-        add_data(received_data["add"])
-    if "remove" in received_data.keys():
-        remove_data(received_data["remove"])
-    update_database()
+    if received_data is not None:
+        if "add" in received_data.keys():
+            add_data(received_data["add"])
+        if "remove" in received_data.keys():
+            remove_data(received_data["remove"])
+        update_database()
+    else:
+        print_error("Error: no data received from the client!")
 
 def add_data(items_to_add):
     global last_id
@@ -199,57 +213,56 @@ def remove_data(items_to_remove):
                 server_data.remove(object)
 
 def update_database():
-    with open(last_id_file, "w") as f:
+    with open(FILE_LAST_ID, "w") as f:
         f.write(str(last_id))
-    with open(server_data_file, 'w') as f:
+    with open(FILE_SERVER_DATA, 'w') as f:
         json.dump(server_data, f, indent=4)
 
 def update_color_theme():
-    with open(color_theme_file, "w") as f:
+    with open(FILE_COLOR_THEME, "w") as f:
         f.write(color_theme)
 
 def md_content(file_name):
-    with open(os.path.join(dirname, "templates", "content", f'{file_name}.md'), 'r', encoding='UTF-8') as f:
+    with open(os.path.join(FOLDER_PROGRAM, "templates", "content", f"{file_name}.md"), 'r', encoding='UTF-8') as f:
         parser = create_markdown(escape=False, plugins=['strikethrough', 'footnotes', 'table'])
         return parser(f.read())
 
 def load_translations():
     global translations
-    with open(translations_file, 'r', encoding='utf-8') as f:
+    with open(FILE_TRANSLATIONS, 'r', encoding='utf-8') as f:
         translations = json.load(f)
 
 def load_last_id():
     global last_id
     try:
-        with open(last_id_file, "r") as f:
+        with open(FILE_LAST_ID, "r") as f:
             try:
                 last_id = int(f.read())
             except ValueError:
-                print(f"Could not parse the content of {last_id_file} to integer, will be reset to zero.")
+                print_error(f"Could not parse the content of '{FILE_LAST_ID}' to integer, will be reset to zero.")
     except FileNotFoundError:
-        print(f"Could not find the file {last_id_file}, it will be created the first time you add something.")
+        print_warning(f"Could not find the file '{FILE_LAST_ID}', it will be created the first time you add something.")
     except Exception as e:
-        print("Last index could not be loaded and was reset, changes will apply on the next edit:", e)
+        print_error(f"Last index could not be loaded and was reset, changes will apply on the next edit:\n{e}")
 
 def load_color_theme():
     global color_theme
     try:
-        with open(color_theme_file, "r") as f:
+        with open(FILE_COLOR_THEME, "r") as f:
             color_theme = f.read()
-            if (color_theme not in [LIGHT_THEME, DARK_THEME]):
-                print(f"The theme '{color_theme}' from the file {color_theme_file} is not valid, will be reset to light.")
-                color_theme = LIGHT_THEME
+            if (color_theme not in [THEME_LIGHT, THEME_DARK]):
+                print_error(f"The theme '{color_theme}' from the file '{FILE_COLOR_THEME}' is not valid, will be reset to light.")
+                color_theme = THEME_LIGHT
                 update_color_theme()
     except FileNotFoundError:
-        print(f"Could not find the file {color_theme_file}, it will be created and the theme will be set to light.")
+        print_warning(f"Could not find the file '{FILE_COLOR_THEME}', it will be created and the theme will be set to light.")
         update_color_theme()
 
 def check_for_updates():
     global local_version, github_version
-    github_version_url = 'https://raw.githubusercontent.com/costantin0/egobalego-at-home/refs/heads/main/data/app_version'
     try:
-        req = requests.get(github_version_url, timeout=10)
-        with open (app_version_file, "r") as f:
+        req = requests.get(UPDATE_CHECK_URL, timeout=10)
+        with open (FILE_APP_VERSION, "r") as f:
             local_version = f.read()
         if req.status_code == requests.codes.ok:
             github_version = req.text
@@ -260,9 +273,17 @@ def check_for_updates():
             else:
                 print("\nUpdate checker: no updates available.\n")
         else:
-            print('\nError during update check: response status code was not "OK".\n')
+            print_warning("Error during update check: response status code was not 'OK'.")
     except Exception as e:
-        print(f"\nError during update check:\n{e}\n")
+        print_warning(f"Error during update check:\n{e}")
+
+def print_warning(message):
+    print(f"{COLOR_WARNING}{message}{COLOR_ENDC}")
+
+def print_error(message):
+    print(f"{COLOR_ERROR}{message}{COLOR_ENDC}")
+
+#endregion
 
 
 if __name__ == '__main__':
@@ -279,7 +300,11 @@ if __name__ == '__main__':
     check_for_updates()
     lang = args_lang
 
+    app_url = f"http://localhost:{args_port}"
+
+    socketio.init_app(app, cors_allowed_origins=app_url)
+
     if args_open:
-        webbrowser.open(f'http://localhost:{args_port}')
+        webbrowser.open(app_url)
 
     socketio.run(app, debug=args_debug, port=args_port)
